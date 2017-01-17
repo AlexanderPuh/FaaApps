5fd1fa806fb7d3428723d67c2c9ac92aa72f95da
AlexanderPuh <alexb8466@gmail.com>
2017-01-17 14:36:55 +0200

    Добавленна кнопка "Напоминания" в меня навигацию и добавлен хэдер

--- app/src/main/java/com/alexb8466gmail/faaapps/MainActivity.java
+++ app/src/main/java/com/alexb8466gmail/faaapps/MainActivity.java
@@ -1,20 +1,26 @@
 package com.alexb8466gmail.faaapps;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
 
+    private static final int LAYOUT=R.layout.activity_main;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
         initToolbar();
+        initNavigationWiew();
     }
+
+
     private void initToolbar()
     {
 toolbar=(Toolbar)findViewById(R.id.toolbar);
@@ -27,5 +33,10 @@ toolbar=(Toolbar)findViewById(R.id.toolbar);
         });
 
 toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationWiew() {
+drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 }
